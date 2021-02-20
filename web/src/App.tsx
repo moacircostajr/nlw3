317ce496b247023db31e5cb138365e1ad7ce3949
Moacir Costa <moacircostajr@gmail.com>
2021-02-20 19:34:42 -0300

    estiliza landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from 'react'
+import './styles/global.css'
+import './styles/pages/landing.css'
+import logoImg from './images/map-marker.svg'
+import { FiArrowRight } from 'react-icons/fi'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+
+        </main>
+
+        <div className="location">
+          <strong>Amontada</strong>
+          <span>Ceará</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </a>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
