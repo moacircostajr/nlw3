c721d01ba2cd9b92a68d063d7cebabb6e3c4a6a5
Moacir Costa <moacircostajr@gmail.com>
2021-02-21 06:28:33 -0300

    conecta com o banco de dados

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,8 @@
 import express from 'express'
+import './database/connection'
 
 const app = express()
-
+app.use(express.json())
 app.get('/users', (request, response) => {
   return response.json({ message: 'Hello World' })
 })
